8dc11e62160ba7606394d043687f306a93124ebb
AjithSimson <ajithsimson@gmail.com>
2024-05-17 20:33:59 +0530

    route structure

--- main.js
+++ main.js
@@ -6,6 +6,33 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+//CRUD functionality of movies
+//c-creating movies
+
+app.get('/movies',()=>{
+
+
+})
+
+//R-reading movies
+
+app.post('/movies',()=>{
+
+
+})
+
+//U-updating movie
+
+app.put('/movie/:id',()=>{
+
+
+})
+
+//D-deleting the movie
+
+app.delete('/movie/:id',()=>{
+
+})
 app.listen(port, () => {
   console.log(`Example app listening on port at http://localhost: ${port}`)
 })
